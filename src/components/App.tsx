904774be787815a4af3325cb6a4255f2abfa09e7
kylemas <34122775+kylemas@users.noreply.github.com>
2019-08-13 21:56:34 -0700

    Example cases (#20)
    * new component for select, cases aren't properly switching
    
    * cases switching properly
    
    * button removed, example cases automatically switch from select
    
    * changed dm example
    
    * SelectInput component renamed to PremisesSelector
    
    * type of handleSubmit changed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,11 @@ import './App.css'
 import React, { useState } from 'react'
 
 import { LeafNode, TreeNode } from '../typings/TreeNode'
-import { decomposeNode, updateNode, parseBranch } from '../util/nodes'
-import { ResolutionModal } from './ResolutionModal'
+import { decomposeNode, parseBranch, updateNode } from '../util/nodes'
 import NodeView from './NodeView'
 import PremiseInput from './PremiseInputProps'
+import { ResolutionModal } from './ResolutionModal'
+import PremisesSelector from './SelectInput'
 
 const examplePremises = 'P->Q,P,~Q'
 const exampleTree: TreeNode | null = parseBranch(examplePremises)
@@ -37,7 +38,7 @@ const App: React.FC = (): JSX.Element => {
     !node.resolved && selectNode(selectedNode === node ? null : node)
   }
 
-  const handleSubmitPremises = (premises: string) => {
+  const handleSubmitPremises = (premises: any) => {
     setTree(parseBranch(premises) || null)
   }
 
@@ -58,6 +59,10 @@ const App: React.FC = (): JSX.Element => {
   return (
     <div className="App">
       <main className="App-main">
+        <PremisesSelector
+          onSubmit={handleSubmitPremises}
+        />
+
         <PremiseInput
           defaultPremises={examplePremises}
           onSubmit={handleSubmitPremises}
@@ -69,13 +74,13 @@ const App: React.FC = (): JSX.Element => {
             selectedNode={selectedNode}
           />
         ) : (
-          '{}'
-        )}
+            '{}'
+          )}
         <ResolutionModal
           {...{ selectedNode, resolveNode, closeBranch, handleClose }}
         />
       </main>
-    </div>
+    </div >
   )
 }
 
