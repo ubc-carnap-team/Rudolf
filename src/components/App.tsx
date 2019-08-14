5df2cfc0a3f8af6be1529a6e222f326cef45a287
McTano <tsmackinlay@gmail.com>
2019-08-13 22:03:28 -0700

    Revert "Example cases (#20)"
    This reverts commit 904774be787815a4af3325cb6a4255f2abfa09e7.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,10 @@ import './App.css'
 import React, { useState } from 'react'
 
 import { LeafNode, TreeNode } from '../typings/TreeNode'
-import { decomposeNode, parseBranch, updateNode } from '../util/nodes'
+import { decomposeNode, updateNode, parseBranch } from '../util/nodes'
+import { ResolutionModal } from './ResolutionModal'
 import NodeView from './NodeView'
 import PremiseInput from './PremiseInputProps'
-import { ResolutionModal } from './ResolutionModal'
-import PremisesSelector from './SelectInput'
 
 const examplePremises = 'P->Q,P,~Q'
 const exampleTree: TreeNode | null = parseBranch(examplePremises)
@@ -38,7 +37,7 @@ const App: React.FC = (): JSX.Element => {
     !node.resolved && selectNode(selectedNode === node ? null : node)
   }
 
-  const handleSubmitPremises = (premises: any) => {
+  const handleSubmitPremises = (premises: string) => {
     setTree(parseBranch(premises) || null)
   }
 
@@ -59,10 +58,6 @@ const App: React.FC = (): JSX.Element => {
   return (
     <div className="App">
       <main className="App-main">
-        <PremisesSelector
-          onSubmit={handleSubmitPremises}
-        />
-
         <PremiseInput
           defaultPremises={examplePremises}
           onSubmit={handleSubmitPremises}
@@ -74,13 +69,13 @@ const App: React.FC = (): JSX.Element => {
             selectedNode={selectedNode}
           />
         ) : (
-            '{}'
-          )}
+          '{}'
+        )}
         <ResolutionModal
           {...{ selectedNode, resolveNode, closeBranch, handleClose }}
         />
       </main>
-    </div >
+    </div>
   )
 }
 
