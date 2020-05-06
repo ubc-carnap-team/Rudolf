60878c5a409cc5ae3d80f982f33ed91c8c7f012a
Tristan MacKinlay <tsmackinlay@gmail.com>
2020-05-06 13:11:38 -0700

    added test file for node utilities

---
+++ src/util/nodes.test.ts
@@ -0,0 +1,15 @@
+import { findResolvedNodes } from './nodes'
+
+describe(findResolvedNodes, () => {
+  it('works with no resolved formulas on branch', () => {
+    fail('TODO')
+  })
+
+  it('works when tree branches', () => {
+    fail('TODO')
+  })
+
+  it('works when tree does not branch', () => {
+    fail('TODO')
+  })
+})
