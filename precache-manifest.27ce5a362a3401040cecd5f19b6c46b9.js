e5b90084f1fd2dbadbdb96fde7e45d0e1dcd2f7a
Tristan MacKinlay <tsmackinlay@gmail.com>
2020-06-17 13:54:05 -0700

    Updates

---
+++ precache-manifest.27ce5a362a3401040cecd5f19b6c46b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c54df6fcde3f0f4991aacf0a28001a",
+    "url": "/Rudolf/index.html"
+  },
+  {
+    "revision": "6eb8dffaf06c4e55d86e",
+    "url": "/Rudolf/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ae6df6fe3e7b7d0d5e6b",
+    "url": "/Rudolf/static/css/main.cdba59ac.chunk.css"
+  },
+  {
+    "revision": "6eb8dffaf06c4e55d86e",
+    "url": "/Rudolf/static/js/2.18edd0ed.chunk.js"
+  },
+  {
+    "revision": "31c3a5abc9661199910ae1ab9246d340",
+    "url": "/Rudolf/static/js/2.18edd0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6df6fe3e7b7d0d5e6b",
+    "url": "/Rudolf/static/js/main.e232b457.chunk.js"
+  },
+  {
+    "revision": "0e31258f6f79576b4cff",
+    "url": "/Rudolf/static/js/runtime-main.0f33440d.js"
+  },
+  {
+    "revision": "a8ace03e6b174f3a221ae352875af169",
+    "url": "/Rudolf/static/media/FiraLogic-Regular.a8ace03e.woff"
+  }
+]);
