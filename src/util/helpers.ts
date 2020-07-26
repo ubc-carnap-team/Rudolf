6a4548506db392c9ef7dc27bffdd9b00d2e13123
Max Becker <max@Maxs-MacBook-Pro.local>
2020-07-25 17:53:27 -0700

    add range to helpers

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -3,3 +3,25 @@ export const isEmptyArray = <T>(
 ): maybeArray is [] => Array.isArray(maybeArray) && maybeArray.length === 0
 
 export const lastEl = <T>(arr: T[]) => arr[arr.length - 1]
+
+const rangeRec = (start: number, stop: number): number[] => {
+  if (start === stop - 1) {
+    return [start]
+  } else {
+    return [start, ...range(start + 1, stop)]
+  }
+}
+
+/**
+ * @param start start of array (inclusive)
+ * @param stop end of array (exclusive)
+ * @returns Array of integers from start to stop.
+ * if start >= stop, returns empty array
+ */
+export const range = (start: number, stop: number): number[] => {
+  if (start < stop) {
+    return rangeRec(start, stop)
+  } else {
+    return []
+  }
+}
