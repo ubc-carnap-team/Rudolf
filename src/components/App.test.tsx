b26e59b55dace224ce5eb5bc289d79ac50d613f0
McTano <tsmackinlay@gmail.com>
2020-09-10 16:54:11 -0700

    bundle app to be consumed by Carnap (#92)
    Export and publish createTree method for use by Carnap. Closes #59, closes #84
    
    * lift checker useEffect
    
    * use parcel instead of webpack
    
    * change compiler option for jsx
    
    * use vanilla css instead of sass
    
    * change package name to "truth-tree"
    
    * configure package.json for npm
    
    * add license field
    
    * remove firalogic font submodule
    
    * removed unused bootrap modal dependencies
    
    * v0.1.5

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Rudolf from './Rudolf'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Rudolf />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
--- src/components/JSONView.tsx
+++ src/components/JSONView.tsx
@@ -1,27 +1,14 @@
 import { TextareaAutosize } from '@material-ui/core'
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useState } from 'react'
 
-import { CustomDispatch, RudolfStore, updateFeedback } from '../RudolfReducer'
+import { CustomDispatch, RudolfStore } from '../RudolfReducer'
 import jsonviewJSS from '../styles/JSONView_styles'
-import { checkTree } from '../util/carnapAdapter'
 
 export const JSONView: FC<RudolfStore & { dispatch: CustomDispatch }> = ({
   tree,
   justifications,
   feedback,
-  dispatch,
 }) => {
-  useEffect(() => {
-    if (window.Carnap) {
-      checkTree(tree, justifications)
-        .then(({ sequent, feedback }) => {
-          return dispatch(updateFeedback({ feedback, sequent }))
-        })
-        .catch(({ message }: Error) => {
-          return dispatch(updateFeedback({ errorMessage: message }))
-        })
-    }
-  }, [dispatch, justifications, tree])
   const classes = jsonviewJSS()
   const [open, setOpen] = useState(false)
   return (
