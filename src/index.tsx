eabb23a04826fdcc4cc8da21ad90dd298b4e8fa8
Tristan MacKinlay <tsmackinlay@gmail.com>
2021-01-04 17:21:32 -0800

    recreated demo page to deploy on github

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import Rudolf from './components/Rudolf'
 import * as serviceWorker from './serviceWorker'
+import { createTrees, WidgetProps } from './lib'
 
 const initialPremises = 'P->Q,P,~Q'
 
-ReactDOM.render(
-  <Rudolf
-    initialPremises={initialPremises}
-    checker={Carnap.checkIchikawaJenkinsSLTableau}
-  />,
-  document.getElementById('root')
-)
+const trees: WidgetProps[] = [
+  {
+    elementId: 'problem-1.1',
+    initialPremises,
+    checker: Carnap.checkIchikawaJenkinsSLTableau,
+  },
+  { elementId: 'problem-1.2', initialPremises: '' },
+]
+
+createTrees(trees)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/lib.ts
+++ src/lib.ts
@@ -1,14 +1,27 @@
 import Rudolf from './components/Rudolf'
 import ReactDOM from 'react-dom'
 import React from 'react'
+import { Checker } from './typings/Checker'
+
+export type WidgetProps = {
+  elementId: string
+  initialPremises: string
+  checker?: Checker
+}
 
 export const createTree = (
   elementId: string,
   initialPremises: string,
-  checker: typeof Carnap.checkIchikawaJenkinsSLTableau = Carnap.checkIchikawaJenkinsSLTableau
+  checker: Checker = Carnap.checkIchikawaJenkinsSLTableau
 ) => {
   ReactDOM.render(
     React.createElement(Rudolf, { initialPremises, checker }),
     document.getElementById(elementId)
   )
+}
+
+export const createTrees = (props: WidgetProps[]) => {
+  props.forEach(({ elementId, initialPremises, checker }) => {
+    createTree(elementId, initialPremises, checker)
+  })
 }
